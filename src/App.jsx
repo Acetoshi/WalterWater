22b66363876a488ba4424b1236235109ee93165f
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-05-16 16:12:22 +0200

    added the feature that can locate users

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { MapContainer, TileLayer } from "react-leaflet";
-
+import { useEffect, useState } from "react";
+import { MapContainer, TileLayer} from "react-leaflet";
+import UserLocation from "./components/UserLocation";
 
 function App() {
+
   return (
     <>
-      <MapContainer center={[47.216671, -1.55]} zoom={14}>
-
+      <MapContainer center={[48.216671, -1.55]} zoom={14}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <UserLocation />
 
       </MapContainer>
     </>
