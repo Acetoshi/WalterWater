c9c80e1a03229ed8101ddb87eadaf614e4e9a21e
Lucasa49 <lucasaksu@gmail.com>
2024-05-16 17:08:10 +0200

    Filterbar CSS fixed button css

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import { MapContainer, TileLayer } from "react-leaflet";
 import "./components/FilterBar";
-import Footer from "./components/FilterBar";
 import "./assets/icomoon/style.css";
+import "./styles/global.css";
+import FilterBar from "./components/FilterBar";
 
 function App() {
   return (
     <>
       <MapContainer center={[47.216671, -1.55]} zoom={14}>
-
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-
       </MapContainer>
-      <Footer/>
+      <FilterBar />
     </>
   );
 }
--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,20 +1,34 @@
-import '../styles/FilterBar.css'
+import "../styles/FilterBar.css";
 
 function FilterBar() {
-    return (
-        <>
-            <div className='FilterBar'></div>
-            <img className='Img1' src="/src/image/005-liste-a-puces.svg" alt="" />
+  return (
+    <>
+      <div className="FilterBar">
+        <button type="button" className="button-filter" id="displayDataButton">
+          <span
+            className="icon-liste-a-puces"
+            aria-label="Affichage en mode liste des points"
+          />
+        </button>
+        <button type="button" className="button-filter">
+          <span
+            className="icon-faucet"
+            aria-label="Affichage des Point d'eau"
+          />
+        </button>
 
-        </>
+        <button type="button" className="button-filter">
+          <span
+            className="icon-restaurant"
+            aria-label="Afficher des lieux pour se restaurer"
+          />
+        </button>
 
-    )
-
-
-
-
-
-
-
+        <button type="button" className="button-filter">
+          <span className="icon-toilette" aria-label="Afficher des toilettes" />
+        </button>
+      </div>
+    </>
+  );
 }
 export default FilterBar;
