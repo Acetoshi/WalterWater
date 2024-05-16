cc82de9fb97c7a6224e065a2359231940ac63ea4
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-05-16 22:35:05 +0200

    fetch function okay, started to implement filter filter functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 import UserLocation from "./components/UserLocation";
-// import FilterBar from "./components/FilterBar"
 import "./assets/icomoon/style.css";
 import "./styles/global.css";
 import FilterBar from "./components/FilterBar";
+import Markers from "./components/Markers";
+import PositionProvider from "./Contexts/PositionProvider";
 
 function App() {
-
+  const [userWantsWater, setUserWantsWater] = useState(true);
   return (
     <>
       <MapContainer center={[48.216671, -1.55]} zoom={14}>
@@ -14,9 +16,13 @@ function App() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <UserLocation />
+        <PositionProvider>
+          <UserLocation />
+          {userWantsWater && <Markers typeOfAmenity={"water"} radius={0.1} />}
+          <Markers typeOfAmenity={"toilets"} radius={0.1} />
+        </PositionProvider>
       </MapContainer>
-      <FilterBar />
+      <FilterBar filters={{userWantsWater, setUserWantsWater}} />
     </>
   );
 }
---
+++ src/components/Markers.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import { Marker, Popup } from "react-leaflet";
+import { usePosition } from "../Contexts/PositionProvider";
+import { faucetIcon, toiletIcon } from "../scripts/icons";
+import { getPoints } from "../scripts/osmUtilities";
+
+export default function Markers({typeOfAmenity,radius}) {
+  const { userLocation } = usePosition();
+  const [points, setPoints] = useState([]);
+
+  let queryDetails=''
+  let icon={}
+
+if (typeOfAmenity==="water"){
+  queryDetails='["amenity"="drinking_water"]';
+  icon=faucetIcon;
+} else if (typeOfAmenity==="toilets"){
+  console.log("toilet query")
+  queryDetails='["amenity"="toilets"]';
+  icon=toiletIcon;
+} 
+
+  useEffect(() => { getPoints(userLocation,radius,setPoints,queryDetails)}, [userLocation]);
+
+  console.log(points);
+
+  return (
+    <ul>
+      {points &&
+        points.map((point) => (
+          <Marker position={[point.lat, point.lon]} icon={icon}>
+            <Popup>Eau potable</Popup>
+          </Marker>
+        ))}
+    </ul>
+  );
+}
