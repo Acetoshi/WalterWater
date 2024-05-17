5fc60d7003b6589a1bc1b2249acfa12e10940d09
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-05-17 12:43:49 +0200

    added easter egg and made the icon switchable

--- src/components/ListView.jsx
+++ src/components/ListView.jsx
@@ -2,15 +2,14 @@
 import { usePosition } from "../Contexts/PositionProvider";
 import InfoCard from "./InfoCard";
 
-const ListView = ({ data, isDisplayed }) => {
-  console.log(data);
-
+const ListView = ({ isDisplayed }) => {
   const { nearbyToilets, nearbyFood, nearbyWater } = usePosition();
+
   return (
     <ul className={isDisplayed ? "list-view" : "list-view hidden"}>
       {nearbyToilets.map((item, index) => (
         <li key={item.id}>
-          <InfoCard pointOfInterest={item}/>
+          <InfoCard pointOfInterest={item} />
         </li>
       ))}
     </ul>
