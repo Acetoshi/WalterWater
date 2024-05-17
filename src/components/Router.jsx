3917910731004cff780d74163a22db53fc6b6b8e
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-05-17 13:05:10 +0200

    made the /info route work.

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,23 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import Home from "../pages/Home"
+import Home from "../pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     errorElement: <h1>404 not found</h1>,
-    children: [
-        {
-        path :'',
-        element:<Home/>
-        },
-         {
-        path :'info',
-        element:<h1>Lucas, fous ton composant ici</h1>
-        },
-    ]
-  }
+  },
+  {
+    path: "/info",
+    element: <h1>Lucas, fous ton composant ici</h1>,
+  },
 ]);
 
 export default router;
