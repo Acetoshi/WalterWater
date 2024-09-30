ea2b8fead5771d861ab15178ad59a536e44640ed
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-10-01 01:07:15 +0200

    compacted MapTracker component

--- src/components/MapTracker.jsx
+++ src/components/MapTracker.jsx
@@ -4,14 +4,11 @@ import { usePosition } from "../Contexts/PositionProvider";
 // This component enables the app to track the center of the map, ie to know where the user is looking.
 export default function MapTracker() {
   const map = useMap();
-  const { userLocation, setMapCenter } = usePosition();
+  const { setMapCenter } = usePosition();
 
   map.on("moveend", () => {
-
     setMapCenter(map.getCenter());
-
   });
-
 
   return null;
 }
