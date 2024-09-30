3d3b832ee1aa1e40ef289865b8fbb422a94b29a7
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-09-30 09:42:25 +0200

    removed the map zoom controls

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
           userWantsFood,
         }}
       />
-      <MapContainer center={[47.216671, -1.55]} zoom={14}>
+      <MapContainer center={[47.216671, -1.55]} zoomControl={false} zoom={14}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
