173be664432b784fe3cc730078402548efdfd0a9
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-10-03 23:08:04 +0200

    added vector icons for the pop-up as well as a more robust grid layout, to be mirrored in the info cards

--- src/components/Markers.jsx
+++ src/components/Markers.jsx
@@ -1,6 +1,11 @@
 import { Marker, Popup } from "react-leaflet";
 import { usePosition } from "../Contexts/PositionProvider";
 import { faucetIcon, toiletIcon, foodIcon } from "../scripts/icons";
+import wheechairIcon from "../assets/icons/wheelchair.svg";
+import moneyIcon from "../assets/icons/money.svg"
+import footstepsIcon from "../assets/icons/footsteps.svg"
+import timeIcon from "../assets/icons/time.svg"
+import "../styles/leafletPopup.css";
 
 export default function Markers({ typeOfAmenity }) {
   const { nearbyPOIs, areaPOIs } = usePosition();
@@ -38,36 +43,42 @@ export default function Markers({ typeOfAmenity }) {
               </h3>
 
               {/* {`${JSON.stringify(point.tags)}`} */}
+              <ul>
 
-              {point.tags.wikimedia_commons && (
-                <img
-                  src={point.tags.wikimedia_commons}
-                  alt="wikimediacommons"
-                ></img>
-              )}
-              {point.tags.wheelchair && (
-                <p>wheelchair access : {point.tags.wheelchair}</p>
-              )}
-              {point.tags.fee && (
-                <p>
-                  {point.tags.fee === "no"
-                    ? "free of charge"
-                    : "access requires a fee"}
-                </p>
-              )}
-              <p>
-                <span
-                  className="icon-emplacement info-logo"
-                  aria-hidden="true"
-                />
-                {`Distance : ${point.distanceKm} km`}
-              </p>
-              <p>
-                <span className="icon-time info-logo" aria-hidden="true" />
-                {`walk time : ${Math.round(
-                  (point.distanceKm * 60) / 4
-                )} mn`}
-              </p>
+                {point.tags.wikimedia_commons && (
+                  <img
+                    src={point.tags.wikimedia_commons}
+                    alt="wikimediacommons"
+                  ></img>
+                )}
+
+                {point.tags.wheelchair && (
+                  <li className="popup-info-row">
+                    <img className="popup-icon" src={wheechairIcon} alt="" />
+                    <p>wheelchair access : {point.tags.wheelchair}</p>
+                  </li>
+                )}
+
+                {point.tags.fee && (
+                  <li className="popup-info-row">
+                    <img className="popup-icon" src={moneyIcon} alt="" />
+                    <p>{point.tags.fee === "no"
+                      ? "free of charge"
+                      : "access requires a fee"}</p>
+                  </li>
+                )}
+
+                <li className="popup-info-row">
+                  <img className="popup-icon" src={footstepsIcon} alt="" />
+                  <p>{`Distance : ${point.distanceKm} km`}</p>
+                </li>
+
+                <li className="popup-info-row">
+                  <img className="popup-icon" src={timeIcon} alt="" />
+                  <p>{`walk time : ${Math.round((point.distanceKm * 60) / 4)} mn`}</p>
+                </li>
+
+              </ul>
             </Popup>
           </Marker>
         ))}
