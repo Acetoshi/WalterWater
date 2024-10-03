55c91065dc6e83f76659aa82ae1a106e39ba2ff3
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-10-03 14:27:59 +0200

    added router and basic privacy policy

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,16 +1,35 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Outlet } from "react-router-dom";
+import Map from '../pages/Map'
 import App from "../App";
+import PrivacyPolicy from "../pages/PrivacyPolicy";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "",
     element: <App />,
     errorElement: <h1>404 not found</h1>,
-  },
-  {
-    path: "/info",
-    element: <h1>Lucas, fous ton composant ici</h1>,
+    children: [
+      {
+        path: "",
+        element: <Map />,
+      },
+      {
+        path: "/about",
+        element: <Outlet />, // Use <Outlet /> as a component
+        children: [
+          {
+            path: "support", 
+            element: <h1>this is the about page</h1>,
+          },
+          {
+            path: "privacy",
+            element: <PrivacyPolicy />,
+          },
+        ],
+      },
+    ],
   },
 ]);
+
 
 export default router;
---
+++ src/pages/Map.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import PositionProvider from "../Contexts/PositionProvider";
+import UserLocation from "../components/UserLocation";
+import Markers from "../components/Markers";
+import MapTracker from "../components/MapTracker";
+import FilterBar from "../components/FilterBar";
+import SearchThisArea from "../components/SearchThisArea";
+import Walter from "../components/Walter";
+import Capybara from "../components/EasterEgg";
+import ListView from "../components/ListView";
+import "../assets/icomoon/style.css";
+import "../styles/global.css";
+
+export default function Map() {
+  const [listIsDisplayed, setListIsDisplayed] = useState(false);
+  const [userWantsWater, setUserWantsWater] = useState(true);
+  const [userWantsToilets, setUserWantsToilets] = useState(true);
+  const [userWantsFood, setUserWantsFood] = useState(false);
+
+  return (
+    <PositionProvider>
+      <Walter />
+      <ListView
+        isDisplayed={listIsDisplayed}
+        filters={{
+          userWantsWater,
+          userWantsToilets,
+          userWantsFood,
+        }}
+      />
+
+      <MapContainer center={[47.216671, -1.55]} zoomControl={false} zoom={14}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        <UserLocation />
+        <MapTracker />
+
+        {userWantsWater && <Markers typeOfAmenity={"water"} />}
+        {userWantsToilets && <Markers typeOfAmenity={"toilets"} />}
+        {userWantsFood && <Markers typeOfAmenity={"food"} />}
+
+        <Capybara />
+      </MapContainer>
+
+      <SearchThisArea />
+      <FilterBar
+        filters={{
+          userWantsWater,
+          setUserWantsWater,
+          userWantsToilets,
+          setUserWantsToilets,
+          userWantsFood,
+          setUserWantsFood,
+          listIsDisplayed,
+          setListIsDisplayed,
+        }}
+      />
+    </PositionProvider>
+  );
+}
