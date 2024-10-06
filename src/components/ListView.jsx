fad950ec27c5b0bf8f794cdd231babfd562bd25e
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-10-06 12:51:26 +0200

    started to work on list view closing : i can now close the list view by clicking anywhere on it, i need to excluse the clicks on the li elements.

--- src/components/ListView.jsx
+++ src/components/ListView.jsx
@@ -7,21 +7,33 @@ const ListView = ({ isDisplayed, setIsDisplayed, filters }) => {
   const { nearbyPOIs } = usePosition();
 
   return (
-    <ul className={isDisplayed ? "list-view" : "list-view hidden"}>
-      {nearbyPOIs
-        .filter(
-          (point) =>
-            (filters.userWantsToilets && point.tags.amenity === "toilets") ||
-            (filters.userWantsWater &&
-              point.tags.amenity === "drinking_water") ||
-            (filters.userWantsFood && point.tags.amenity === "restaurant")
-        )
-        .map((point) => (
-          <li key={point.id}>
-            <InfoCard point={point} setIsDisplayed={setIsDisplayed} />
-          </li>
-        ))}
-    </ul>
+    <button
+      role="button"
+      aria-label="close list view"
+      onClick={(event) => {
+        console.log(event.target);
+        setIsDisplayed(false);
+      }}
+      className={
+        isDisplayed ? "list-view-container" : "list-view-container hidden"
+      }
+    >
+      <ul className={isDisplayed ? "list-view" : "list-view hidden"}>
+        {nearbyPOIs
+          .filter(
+            (point) =>
+              (filters.userWantsToilets && point.tags.amenity === "toilets") ||
+              (filters.userWantsWater &&
+                point.tags.amenity === "drinking_water") ||
+              (filters.userWantsFood && point.tags.amenity === "restaurant")
+          )
+          .map((point) => (
+            <li key={point.id}>
+              <InfoCard point={point} setIsDisplayed={setIsDisplayed} />
+            </li>
+          ))}
+      </ul>
+    </button>
   );
 };
 
