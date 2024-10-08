b030d8720fbeeb94fe32e4958225334d68344564
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-10-09 00:20:17 +0200

    inserted a self-destruct mechanism in the splashscreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import {useState,useEffect} from "react"
 import { Outlet } from "react-router-dom";
 import Droplet from "./components/Droplet";
 
 export default function App() {
+
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <Droplet />
+      {isVisible && <Droplet />}
       <Outlet />
     </>
   );
