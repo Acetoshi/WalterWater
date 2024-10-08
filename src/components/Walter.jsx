1e4475f26cddefbe176e02c05a446378504c4e08
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-10-08 13:37:59 +0200

    now walter will auto-shut up after 3.5s

--- src/components/Walter.jsx
+++ src/components/Walter.jsx
@@ -10,7 +10,6 @@ function Walter() {
   const { areaPOIs } = usePOIs();
 
   const [walterIsVisible, setWalterIsVisible] = useState(false);
-  const [tip, setTip] = useState("");
   const [message, setMessage] = useState("");
 
   const failMessages = [
@@ -20,11 +19,29 @@ function Walter() {
     "Nothing here yet, but remember, even the best explorers faced a few empty maps before striking gold!",
   ];
 
+  const tips = [
+    "Welcome, dear explorer! Below are some buttons to quench your thirst during your jolly good adventure!!",
+    "Drink like a fish, but stick to water, not beer. Dehydration isn’t exactly a laugh on a hike!",
+    "A chocolate bar is lovely, but do throw in some dried fruit as well. Eating is fab; staying energised is even better!",
+    "Don't forget your water bottle! You’re not a cactus; you’ll need some proper hydration!",
+    "Relieve yourself away from the paths... unless you fancy the inquisitive stares of squirrels.",
+    "Have a light snack before you head out; otherwise, you’ll be rolling down the trails instead of walking!",
+    "Nuts are splendid for breaks: energising and crunchy! Chips, however, are a hard pass!",
+    "Hydrate regularly, dear. This isn’t a race; it’s a leisurely hike, so no need to cart around a water tank!",
+    "A decent sandwich is the secret to happiness on a hike. No need for fancy foie gras; a good old ham and butter will do nicely!",
+    "Need a pick-me-up? Have a square of dark chocolate. In need of a breather? Sit back and soak in the scenery.",
+    "Improvised loo? Dig a hole and cover it up afterwards. Bears appreciate a tidy environment, thank you very much!",
+  ];
+
   const walterSays = (something) => {
     setWalterIsVisible(true);
     setMessage(something);
-    setTimeout(() => setWalterIsVisible(false), 3000);
+    setTimeout(() => setWalterIsVisible(false), 3500);
   };
+
+  const randomInt = (max)=>{
+    return Math.floor(Math.random() * max)
+  }
 
   //warn the user when his search didn't find anything
   useEffect(() => {
@@ -38,25 +55,21 @@ function Walter() {
     }
     if (areaPOIs.length === 0) {
       walterSays(
-        failMessages[Math.floor(Math.random() * 0.99 * failMessages.length)]
+        failMessages[randomInt(failMessages.length-1)]
       );
     }
   }, [areaPOIs]);
 
   // au chargement
   useEffect(() => {
-    //affichage bulle infos
-
     // Déclencher l'animation après le montage du composant
     const firstLoad = localStorage.getItem("firstLoad");
     if (!firstLoad) {
       // First load, show the first element and set the flag in localStorage
-      setTip(tips[0].tip);
       localStorage.setItem("firstLoad", "true");
     } else {
       // Subsequent loads, show a random element
-      const randomIndex = Math.floor(Math.random() * tips.length);
-      setTip(tips[randomIndex].tip);
+      walterSays(tips[randomInt(tips.length-1)]);
     }
   }, []);
 
