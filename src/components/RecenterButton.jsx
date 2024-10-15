e8f1f1de42b669eda0e7bcaa49473001b3b0a12f
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-10-15 18:26:53 +0200

    did the same with the recenter button

--- src/components/RecenterButton.jsx
+++ src/components/RecenterButton.jsx
@@ -3,23 +3,25 @@ import "../styles/recenterButton.css";
 
 export default function RecenterButton() {
   const { mapPosition, recenterIsNeeded, setrecenterIsNeeded } = usePosition();
-  
+
   return (
-    <button
-      id="recenter-button"
-      type="button"
-      className="button-feedback"
-      onClick={() => setrecenterIsNeeded(!recenterIsNeeded)}
-      disabled={(mapPosition.distanceFromUser<=0.05)}
-    >
-      <img
-        src={
-          mapPosition.distanceFromUser >= 0.2
-            ? "/icons/recenterWhiteEmpty.png"
-            : "/icons/recenterWhiteFull.png"
-        }
-        alt="recenter the map on your position"
-      ></img>
-    </button>
+    <div id="recenter-button-container">
+      <button
+        id="recenter-button"
+        type="button"
+        className={mapPosition.distanceFromUser >= 0.05 && "button-feedback"}
+        onClick={() => setrecenterIsNeeded(!recenterIsNeeded)}
+        disabled={mapPosition.distanceFromUser <= 0.05}
+      >
+        <img
+          src={
+            mapPosition.distanceFromUser >= 0.2
+              ? "/icons/recenterWhiteEmpty.png"
+              : "/icons/recenterWhiteFull.png"
+          }
+          alt="recenter the map on your position"
+        ></img>
+      </button>
+    </div>
   );
 }
