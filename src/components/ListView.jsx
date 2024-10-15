a58ffd61a1fdcec387d3617e3692050779d8662a
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-10-15 19:37:07 +0200

    CSS is hard and a i have a strange blinking behaviour, maybe the useMemo isn't such a good idea anymore for POI Details ?

--- src/components/ListView.jsx
+++ src/components/ListView.jsx
@@ -3,7 +3,6 @@ import { usePOIs } from "../Contexts/PointsOfInterestProvider";
 import InfoCard from "./InfoCard";
 import "../styles/listview.css";
 
-
 const ListView = ({ isDisplayed, setIsDisplayed }) => {
   const { POIs } = usePOIs();
 
@@ -21,16 +20,17 @@ const ListView = ({ isDisplayed, setIsDisplayed }) => {
         isDisplayed ? "list-view-container" : "list-view-container hidden"
       }
     >
-      <ul className={isDisplayed ? "list-view" : "list-view hidden"}>
-        <VariableSizeList
-          height={1000}
-          width={400}
-          itemCount={POIs.length}
-          itemSize={()=>200}
-        >
-            {({index})=><InfoCard point={POIs[index]} setIsDisplayed={setIsDisplayed} />}
-        </VariableSizeList>
-      </ul>
+      <VariableSizeList
+        className={isDisplayed ? "list-view-container" : "list-view-container hidden"}
+        height={1000}
+        width={400}
+        itemCount={POIs.length}
+        itemSize={()=>200}
+      >
+        {({ index }) => (
+          <InfoCard point={POIs[index]} setIsDisplayed={setIsDisplayed} />
+        )}
+      </VariableSizeList>
     </button>
   );
 };
