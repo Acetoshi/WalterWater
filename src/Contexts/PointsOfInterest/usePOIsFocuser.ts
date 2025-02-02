b8c037d38a7b7371ef8391dbb753101d11748f82
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2025-02-02 18:04:46 +0100

    FEATURE/Markers : solved fuzzy behaviour of markers by changing the dependency array in useCallback

--- src/Contexts/PointsOfInterest/usePOIsFocuser.ts
+++ src/Contexts/PointsOfInterest/usePOIsFocuser.ts
@@ -1,19 +1,23 @@
 import { useEffect } from 'react';
 import { useMap } from 'react-leaflet';
 import usePOIs from './usePOIs';
-
-//TODO convert to a hook
+import usePosition from '../Position/usePosition';
 
 export default function usePOIsFocuser() {
   const { targetPOIPosition } = usePOIs();
+  const { mapPosition } = usePosition();
   const map = useMap();
 
   // this enables the list view to move the map to a target POI
   useEffect(() => {
+    // first, calculate the view height
+    const latOffset = (mapPosition.bounds.maxLat - mapPosition.bounds.minLat) / 4;
+
     if (targetPOIPosition.lat !== 0) {
       // this prevents a teleport to NULL island
       map.closePopup();
-      map.flyTo(targetPOIPosition, 18, { duration: 1 });
+      const zoom = map.getZoom();
+      map.flyTo({ lat: targetPOIPosition.lat + latOffset, lng: targetPOIPosition.lng }, zoom, { duration: 1 });
     }
   }, [targetPOIPosition]);
 }
